13807a72e868b2da07b4404d1b2774050c1ab371
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-10-06 21:31:25 +0600

    Added adopt button modal using javascript

--- js/peddy.js
+++ js/peddy.js
@@ -19,7 +19,7 @@ const displayPets = (pets) => {
     const noSearchMessage = document.getElementById('no-search-message');
     const cardsContainer = document.getElementById("cards-container");
     cardsContainer.textContent = "";
-    
+
     if (pets.length === 0) {
         loadingSpinner(false);
         noSearchMessage.classList.remove('hidden');
@@ -93,17 +93,18 @@ const displayPets = (pets) => {
                                 d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
                         </svg>
                     </button>
-                    <button class="btn bg-white border border-[#0E7A8126] text-[#0E7A81] font-extrabold">Adopt</button>
+                    <button onclick="adoptModal(this)" class="btn bg-white border border-[#0E7A8126] text-[#0E7A81] font-extrabold">Adopt</button>
+
                     <button onclick="openModal('${pet.petId
             }')" class="btn bg-white border border-[#0E7A8126] text-[#0E7A81] font-extrabold">Details</button>
                 </div>
             </div>
         `;
-        
+
         cardsContainer.append(card);
     });
 
-    loadingSpinner(false) 
+    loadingSpinner(false)
 };
 
 // load buttons function
@@ -147,7 +148,7 @@ const displayButton = (category) => {
 // category buttons
 const categoryWiseButton = async (categoryButtonType) => {
     loadingSpinner(true);
-    
+
 
     const cardsContainer = document.getElementById("cards-container");
     const likeImageContainer = document.getElementById("like-image-container");
@@ -284,7 +285,7 @@ const loadingSpinner = (isLoading) => {
     } else {
         spinner.classList.add('hidden');
     }
-    
+
 }
 
 // sorting function
@@ -300,7 +301,27 @@ const sortedButton = async () => {
     sortPetByPrice(pets);
 }
 
+let count = 3
+const adoptModal = (button) => {
+    const myModal = document.getElementById('my_modal_4');
+    const countdown = document.getElementById('countdown');
 
+    button.disabled = true;
+
+    count = 3
+    countdown.textContent = count;
+    myModal.showModal();
+
+    const countdownInterval = setInterval(() => {
+        count--;
+        countdown.textContent = count;
+
+        if (count === 0) {
+            clearInterval(countdownInterval);
+            myModal.close(); 
+        }
+    }, 1000);
+}
 
 loadPets();
 loadButtons();
