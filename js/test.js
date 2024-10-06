3c1c816a38b25ad9bdb6fb684131dba12c000c11
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-10-06 12:45:56 +0600

    Added category buttons functionalities dynamically using javascript

--- js/test.js
+++ js/test.js
@@ -1,40 +1,35 @@
 // load pets function
 const loadPets = async () => {
-    const res = await fetch(
-        "https://openapi.programming-hero.com/api/peddy/pets"
-    );
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
     const data = await res.json();
     const pets = data.pets;
     displayPets(pets);
-};
+}
 
 // display pets function
 const displayPets = (pets) => {
-    const cardsContainer = document.getElementById("cards-container");
-    cardsContainer.textContent = "";
+    const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.textContent = '';
 
-    pets.forEach((pet) => {
-        const card = document.createElement("div");
-        card.classList.add("card", "border", "border-[#CFCFCF]");
-
+    pets.forEach(pet => {
+        const card = document.createElement('div');
+        card.classList.add('card', 'border', 'border-[#CFCFCF]');
+        
         card.innerHTML = `
             <div class="w-11/12 mx-auto mt-5">
             <figure class="w-full rounded-lg">
-            <img class="w-full object-cover" src="${pet.image}" alt="${pet.image
-            } image" />
+            <img class="w-full object-cover" src="${pet.image}" alt="${pet.image} image" />
                 </figure>
             </div>
             <div class="card-body p-0 w-11/12 mx-auto">
-                <h2 class="card-title mt-5">${pet.pet_name || "Not available"
-            }</h2>
+                <h2 class="card-title mt-5">${pet.pet_name || 'Not available'}</h2>
                 <div class="flex gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                     </svg>
-                    <p class="text-[#131313B3]">Breed: ${pet.breed || "Not available"
-            }</p>
+                    <p class="text-[#131313B3]">Breed: ${pet.breed || 'Not available'}</p>
                 </div>
                 <div class="flex gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -42,8 +37,7 @@ const displayPets = (pets) => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                     </svg>
-                    <p class="text-[#131313B3]">Birth: ${pet.date_of_birth || "Not available"
-            }</p>
+                    <p class="text-[#131313B3]">Birth: ${pet.date_of_birth || 'Not available'}</p>
                 </div>
                 <div class="flex gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -51,8 +45,7 @@ const displayPets = (pets) => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                     </svg>
-                    <p class="text-[#131313B3]">Gender: ${pet.gender || "Not available"
-            }</p>
+                    <p class="text-[#131313B3]">Gender: ${pet.gender || 'Not available'}</p>
                 </div>
                 <div class="flex gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -60,16 +53,14 @@ const displayPets = (pets) => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-                    <p class="text-[#131313B3]">Price : ${pet.price || "Not available"
-            }$</p>
+                    <p class="text-[#131313B3]">Price : ${pet.price || 'Not available'}$</p>
                 </div>
                 <!-- divider -->
                 <div class="divider text-[#1313131A]"></div>
 
                 <!-- buttons functionalities -->
                 <div class="flex justify-between items-center gap-1 md:gap-0 pb-5">
-                    <button onclick="likeButton('${pet.petId}', '${pet.image
-            }')" class="btn bg-white border border-[#0E7A8126]">
+                    <button onclick="likeButton('${pet.petId}', '${pet.image}')" class="btn bg-white border border-[#0E7A8126]">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -77,95 +68,97 @@ const displayPets = (pets) => {
                         </svg>
                     </button>
                     <button class="btn bg-white border border-[#0E7A8126] text-[#0E7A81] font-extrabold">Adopt</button>
-                    <button onclick="openModal('${pet.petId
-            }')" class="btn bg-white border border-[#0E7A8126] text-[#0E7A81] font-extrabold">Details</button>
+                    <button onclick="openModal('${pet.petId}')" class="btn bg-white border border-[#0E7A8126] text-[#0E7A81] font-extrabold">Details</button>
                 </div>
             </div>
-        `;
+        `
 
         cardsContainer.append(card);
     });
-};
+}
+
 
 // load buttons function
 const loadButtons = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/peddy/categories");
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
     const data = await res.json();
     const categories = data.categories;
-    categories.forEach((category) => {
+    categories.forEach(category => {
         displayButton(category);
     });
-};
+}
 
 // display button function
 const displayButton = (category) => {
-    const buttonContainer = document.getElementById("button-container");
-    const categoryButton = document.createElement("button");
+    const buttonContainer = document.getElementById('button-container');
+    const categoryButton = document.createElement('button');
 
     categoryButton.classList.add('btn', 'md:w-1/5', 'font-bold');
     categoryButton.textContent = category.category;
 
-    categoryButton.addEventListener("click", async () => {
+    // Add click event listener
+    categoryButton.addEventListener('click', async () => {
         await categoryWiseButton(category.category);
     });
 
-    const categoryIcon = document.createElement("img");
+    // Add icon (improved method)
+    const categoryIcon = document.createElement('img');
     categoryIcon.src = category.category_icon;
     categoryIcon.alt = `${category.category} icon`;
-    categoryIcon.style.width = "20px";
-    categoryIcon.style.height = "20px";
+    categoryIcon.style.width = '20px';
+    categoryIcon.style.height = '20px';
     categoryButton.prepend(categoryIcon); // Add icon before text
 
+
     buttonContainer.appendChild(categoryButton);
-};
+}
 
 // category buttons
 const categoryWiseButton = async (categoryButtonType) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryButtonType}`);
     const data = await res.json();
-    const categoryData = data.data;
-    displayPets(categoryData);
+    const categoryData = data.data
+    displayPets(categoryData)
 };
+
+
 
 // like button function
 const likeButton = (petId, image) => {
-    const likeImageContainer = document.getElementById("like-image-container");
+    const likeImageContainer = document.getElementById('like-image-container');
 
-    const imageWrapper = document.createElement("div");
-    imageWrapper.classList.add("aspect-square", "relative");
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('aspect-square', 'relative');
 
-    const likeImage = document.createElement("img");
+    const likeImage = document.createElement('img');
     likeImage.src = image;
     likeImage.alt = `${image} image`;
-    likeImage.classList.add("rounded-lg");
+    likeImage.classList.add('rounded-lg');
 
     imageWrapper.appendChild(likeImage);
     likeImageContainer.appendChild(imageWrapper);
-};
+}
 
 // show modal function
 const openModal = async (petId) => {
     try {
-        const res = await fetch(
-            `https://openapi.programming-hero.com/api/peddy/pet/${petId}`
-        );
+        const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`);
         const data = await res.json();
         const petData = data.petData;
         displayPetDetails(petData);
     } catch (error) {
-        console.error("Error occurs", error);
+        console.error('Error occurs', error);
     }
-};
+}
 
 // display modal function
 const displayPetDetails = (petData) => {
-    const showAllDetails = document.getElementById("my_modal_5");
+    const showAllDetails = document.getElementById('my_modal_5');
     showAllDetails.innerHTML = `
                 <div class="modal-box w-11/12 mx-auto p-10">
                     <div class="">
                         <figure class="w-full text-center">
-                            <img class="w-full rounded-lg" src="${petData.image
-        }" alt="${petData.image}" class="w-1/2 mx-auto" />
+                            <img class="w-full rounded-lg" src="${petData.image}" alt="${petData.image}" class="w-1/2 mx-auto" />
                         </figure>
                     </div>
 
@@ -178,8 +171,7 @@ const displayPetDetails = (petData) => {
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                             </svg>
-                            <p class="text-[#131313B3]">Breed: ${petData.breed || "Not available"
-        }</p>
+                            <p class="text-[#131313B3]">Breed: ${petData.breed || 'Not available'}</p>
                         </div>
                         <div class="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -187,8 +179,7 @@ const displayPetDetails = (petData) => {
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                             </svg>
-                            <p class="text-[#131313B3]">Birth: ${petData.date_of_birth || "Not available"
-        }</p>
+                            <p class="text-[#131313B3]">Birth: ${petData.date_of_birth || 'Not available'}</p>
                         </div>
                         <div class="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -196,8 +187,7 @@ const displayPetDetails = (petData) => {
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                             </svg>
-                            <p class="text-[#131313B3]">Gender: ${petData.gender || "Not available"
-        }</p>
+                            <p class="text-[#131313B3]">Gender: ${petData.gender || 'Not available'}</p>
                         </div>
                         <div class="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -205,16 +195,14 @@ const displayPetDetails = (petData) => {
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
-                            <p class="text-[#131313B3]">Price : ${petData.price || "Not available"
-        }$</p>
+                            <p class="text-[#131313B3]">Price : ${petData.price || 'Not available'}$</p>
                         </div>
                         <div class="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
 
-                            <p class="text-[#131313B3]">Vaccinated status: ${petData.gender || "Not available"
-        }</p>
+                            <p class="text-[#131313B3]">Vaccinated status: ${petData.gender || 'Not available'}</p>
                         </div>
                     </div>
 
@@ -223,7 +211,7 @@ const displayPetDetails = (petData) => {
 
                     <h3 class="font-bold pt-2">Details Information</h3>
                     <p class="py-4 text-[#706F6F]">
-                        ${petData.pet_details || "Not available"}
+                        ${petData.pet_details || 'Not available'}
                     </p>
                     <div class="modal-action">
                         <form method="dialog" class="w-full">
@@ -231,10 +219,10 @@ const displayPetDetails = (petData) => {
                         </form>
                     </div>
                 </div>
-    `;
+    `
 
     my_modal_5.showModal();
-};
+}
 
 loadPets();
 loadButtons();
