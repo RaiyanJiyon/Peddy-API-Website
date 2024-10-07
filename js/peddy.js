cb836e1da3b61258734b487ffbe330f7bc3910fa
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-10-08 05:55:45 +0600

    Added only year and fixed Vaccinated status in details modal

--- js/peddy.js
+++ js/peddy.js
@@ -60,7 +60,7 @@ const displayPets = (pets) => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                     </svg>
-                    <p class="text-[#131313B3]">Birth: ${pet.date_of_birth || "Not available"}</p>
+                    <p class="text-[#131313B3]">Birth: ${new Date(pet.date_of_birth).getFullYear() || "Not available"}</p>
                 </div>
                 <div class="flex gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -224,7 +224,7 @@ const displayPetDetails = (petData) => {
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                             </svg>
-                            <p class="text-[#131313B3]">Birth: ${petData.date_of_birth || "Not available"}</p>
+                            <p class="text-[#131313B3]">Birth: ${new Date(petData.date_of_birth).getFullYear() || "Not available"}</p>
                         </div>
                         <div class="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -247,7 +247,7 @@ const displayPetDetails = (petData) => {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
 
-                            <p class="text-[#131313B3]">Vaccinated status: ${petData.gender || "Not available"}</p>
+                            <p class="text-[#131313B3]">Vaccinated status: ${petData.vaccinated_status || "Not available"}</p>
                         </div>
                     </div>
 
